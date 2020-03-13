ada339d546538ae59c13daf8bf7033f6647b4760
Tal Weiss <tweiss747@gmail.com>
2020-03-13 18:55:09 +0200

    added file explorer button

--- src/SideBar/SideBar.tsx
+++ src/SideBar/SideBar.tsx
@@ -2,27 +2,48 @@ import React from "react";
 import IProps from "../Interfaces/IProps";
 import IState from "../Interfaces/IState";
 import * as s from './SideBar.css';
-import Button from 'react-bootstrap/Button';
-
-import IoIosOpen from 'react-icons/io';
 export default class SideBar extends React.Component<IProps,IState>{
+    fileReference: React.RefObject<HTMLInputElement>;
     constructor(props){
         super(props);
         
+        // method binds
         this.openFile = this.openFile.bind(this);
         this.speakText = this.speakText.bind(this)
-    }
-    openFile(){console.log("Open Button Pressed")}
+        this.fileReference = React.createRef();
 
+
+
+    }
+    // this will use the docx reader installed from npm
+    openFile(event){
+        event.preventDefault();
+        console.log("File name")
+        console.log(this.fileReference.current.files[0]);
+    }
+    
+    // this will utilize a text to speech api.
     speakText(){console.log("Speech Button Pressed")}
 
 
     render(){
         return(
             <div className={s.sideBarStyling}>
-                <div className={s.btnStack}>
-                    <button className={[s.sideBarBtn, s.primary].join(' ')} onClick={this.openFile}>Open</button>
-                </div>
+                
+{/*                     
+                    <button className={[s.sideBarBtn, s.primary].join(' ')} onClick={this.openFile}>Open</button> */}
+                    <form onSubmit={this.openFile}>
+
+                        <div className={s.btnStack}>
+                            {/* Fix styling for the input tag */}
+                            <input  type="file" ref={this.fileReference}/>
+                        </div>
+
+                        <div className={s.btnStack}>
+                            <button className={[s.sideBarBtn,s.primary].join(' ')} type="submit">Open</button>
+                        </div>
+                    </form>
+            
                 <div className={s.btnStack}>
                     <button className={[s.sideBarBtn, s.primary].join(' ')} onClick={this.speakText}>Speech</button>
                 </div>
