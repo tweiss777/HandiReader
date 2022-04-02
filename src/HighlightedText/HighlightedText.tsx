0087c8a3b602a36766c46a1f8b47bb941778f4fb
Tal Weiss <tweiss747@gmail.com>
2022-04-02 12:11:23 +0300

    converted HighlightedText to function component

--- src/HighlightedText/HighlightedText.tsx
+++ src/HighlightedText/HighlightedText.tsx
@@ -1,45 +1,32 @@
-import React, { MouseEventHandler } from 'react';
+import React, { MouseEventHandler, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import IProps from '../Interfaces/IProps';
 import IState from '../Interfaces/IState';
 import * as h from './HighlightedText.css';
 
-export default class HighlightedText extends React.Component<IProps,IState>{
 
-
-    constructor(props){
-        super(props);
-        this.state = {
-            isSpeaking: false
-            
-        }
-    }
-
-
-    UnHighlightText = (event) => {
+export default function HighlightedText(props: IProps){
+    const [isSpeaking,setIsSpeaking] = useState<boolean>(false)
+    
+    var text: string = props.highlightedText as string;
+    
+    const UnHighlightText = (event) => {
         // console.log(event.keyCode);
         console.log("un-highlighting text");
-        this.props.unhighlightText();
+        props.unhighlightText();
         
     }
 
-    speakText: MouseEventHandler = () => {
+    const speakText: MouseEventHandler = () => {
         console.log("speaking text")
     }
+    // testText should be used for debugging purposes
+    // var testText: string = "Test string"
+    return(
+        <div className={h.highlightContainer}>
+            <p onClick={UnHighlightText} className={h.highlightedText}>{text}</p>
+            <Button onClick={speakText}>Speak text</Button>
 
-    render(){
-        var text: string = this.props.highlightedText as string;
-
-        // testText should be used for debugging purposes
-        // var testText: string = "Test string"
-        return(
-            <div className={h.highlightContainer}>
-                <p onClick={this.UnHighlightText} className={h.highlightedText}>{text}</p>
-                <Button onClick={this.speakText}>Speak text</Button>
-
-            </div>
-        ) 
-        
-    }
-
+        </div>
+    ) 
 }
